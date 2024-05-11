516862b04740ade4417fbd93e257a57afd8fdcd3
SimuhMamad <muhammadabdurrahman891@gmail.com>
2024-05-11 11:27:48 +0700

    vercel

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const bookRouter = require('./routes/book.router')
 
 app.use("/", bookRouter)
 
-app.get("/", (req, res) => {
-    res.sendFile("/login.html", {root: __dirname});
-})
+// app.get("/", (req, res) => {
+//     res.sendFile("/login.html", {root: __dirname});
+// })
 
-app.get("/registrasi.html", (req, res) => {
-    res.sendFile("/registrasi.html", {root: __dirname});
-})
+// app.get("/registrasi.html", (req, res) => {
+//     res.sendFile("/registrasi.html", {root: __dirname});
+// })
 
 app.listen(process.env.PORT, () => console.log("Server is running on port 3000"))
--- routes/book.router.js
+++ routes/book.router.js
@@ -3,6 +3,10 @@ const router = express.Router()
 
 const bookController = require('../controllers/book.controller')
 
+router.get("/", (req, res) => {
+    res.sendFile("../login.html", {root: __dirname});
+})
+
 router.get("/get-User", bookController.getUser)
 router.post("/postPass", bookController.checkUser)
 router.post("/postRegis", bookController.insertUser)
