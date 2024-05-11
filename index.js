f9c727d948b4bae4b72059a4d5ae87eb29a47897
SimuhMamad <muhammadabdurrahman891@gmail.com>
2024-05-11 12:24:16 +0700

    vercel

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const bookRouter = require('./routes/book.router')
 app.use("/", bookRouter)
 
 app.get("/", (req, res) => {
-    res.sendFile("/login.html", {root: path.join(__dirname, "public")});
+    res.sendFile("/public/login.html", {root: __dirname});
 })
 
 app.get("/registrasi.html", (req, res) => {
-    res.sendFile("/registrasi.html", {root: __dirname});
+    res.sendFile("/public/registrasi.html", {root: __dirname});
 })
 
 app.listen(process.env.PORT, () => console.log("Server is running on port 3000"))
